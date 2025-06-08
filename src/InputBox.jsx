8bd1043c17e0f1beebe3d78e7cc6f1311ff15017
scaler <>
2025-06-08 14:02:08 +0000

    docs: SUBMISSION_COMMIT

--- src/InputBox.jsx
+++ src/InputBox.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function InputBox(props) {
-  const [addTask] =props;
+  const {addTask} =props;
   const [task,setTask]= useState('');
 
   const handleInput = (e) => {
